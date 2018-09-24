62f8f5dc7dab4f76b111dbb0d817f367dc090f88
A. Mauricio Ospina <Andres.M.Ospina.-ND@disney.com>
2018-09-24 13:07:10 -0500

    Add annotation

--- DesignPatterns_Katas/src/co/com/factorymethod/ConcreteProduct.java
+++ DesignPatterns_Katas/src/co/com/factorymethod/ConcreteProduct.java
@@ -3,6 +3,7 @@ package co.com.factorymethod;
 public class ConcreteProduct implements Product {
 	
 	// Operation.
+	@Override
 	public void operation() {
         System.out.println("An operation of this product.");
     }
